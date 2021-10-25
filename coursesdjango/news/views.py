631cb3a387a628bcc1bda9c6bb3a9bbf47ce5cfc
ruleona <61205714+ruleona@users.noreply.github.com>
2021-10-25 15:53:26 +0300

    Change styles for the project

--- coursesdjango/news/views.py
+++ coursesdjango/news/views.py
@@ -3,31 +3,15 @@ from .models import Article
 from .forms import ArticleForm  # Импортируем форму
 from django.views.generic import DetailView, UpdateView, DeleteView # Класс, необходимый для динамически отображаемых страниц, на основе которого создадим свой новый класс
 
+
 def news_home(request):
     news = Article.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
 
-class NewsDetailView(DetailView):
-    model = Article
-    template_name = 'news/details_view.html'
-    context_object_name = 'article' #название ключа, по которому будем передавать запись внутрь шаблона
-
-
-class NewsUpdateView(UpdateView):
-    model = Article
-    template_name = 'news/create.html'
-
-    form_class = ArticleForm
-
-
-class NewsDeleteView(DeleteView):
-    model = Article
-    success_url = '/news/'
-    template_name = 'news/news-delete.html'
-
 
 def create(request):
     # Получаем данные из формы:
+    error = ''
     if request.method == 'POST':
         form = ArticleForm(request.POST) #Создается объект класса ArticleForm, содержащий все данные, которые ввел пользователь
         if form.is_valid(): # Метод класса ModelForm, проверяющий, корректно ли введены данные.
@@ -41,3 +25,23 @@ def create(request):
         'error': error
     }
     return render(request, 'news/create.html', data)
+
+
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = 'news/details_view.html'
+    context_object_name = 'article' #название ключа, по которому будем передавать запись внутрь шаблона
+
+
+class NewsUpdateView(UpdateView):
+    model = Article
+    template_name = 'news/create.html'
+    form_class = ArticleForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Article
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
+
+
