41f43aa9855e66dc12a5b7331003f55a6da1f782
ruleona <61205714+ruleona@users.noreply.github.com>
2021-10-26 18:31:19 +0300

    Add contacts

--- coursesdjango/lesson_one/urls.py
+++ coursesdjango/lesson_one/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
+
 from . import views
 
 urlpatterns = [
     path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
     re_path(r'^$', views.index, name='home'),
 ]
--- coursesdjango/lesson_one/views.py
+++ coursesdjango/lesson_one/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
@@ -8,12 +8,16 @@ def index(request):
         'title': 'Главная страница',
         'values': ['Listen', 'to', 'your', 'heart.'],
         'obj': {'персона': 'человек',
-              'марафон': 'гонка бегунов длиной около 26 миль',
-              'противостоять': 'оставаться сильным, несмотря на давление',
-              'бежать': 'двигаться со скоростью'}
+                'марафон': 'гонка бегунов длиной около 26 миль',
+                'противостоять': 'оставаться сильным, несмотря на давление',
+                'бежать': 'двигаться со скоростью'}
     }
     return render(request, 'lesson_one/index.html', data)
 
 
 def about(request):
     return render(request, 'lesson_one/about.html')
+
+
+def contacts(request):
+    return render(request, 'lesson_one/contacts.html')
