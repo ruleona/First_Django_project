13911ef503d19f27ef23d5416a4ddc36770a71ec
ruleona <61205714+ruleona@users.noreply.github.com>
2021-11-23 17:36:37 +0300

    'deploy'

--- coursesdjango/coursesdjango/settings.py
+++ coursesdjango/coursesdjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+from pathlib import Path
 
 import dj_database_url
 import environ
@@ -19,13 +20,14 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ['SECRET_KEY']
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -82,6 +84,9 @@ WSGI_APPLICATION = 'coursesdjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+if os.environ.get('DATABASE_URL', None):
+    os.environ["DATABASE_URL"] = "postgres://ruleona:1349@localhost:5432/coursesdjango"
 
 DATABASES = {
     'default': {
